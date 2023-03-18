f0f9c9f3adb417293aaf4b75a9993e789f059040
Nahid Islam <75819596+nahidtislam@users.noreply.github.com>
2023-03-18 13:31:25 +0000

    Initial Commit

---
+++ pull me urbanD/pull_me_urbanDApp.swift
@@ -0,0 +1,17 @@
+//
+//  pull_me_urbanDApp.swift
+//  pull me urbanD
+//
+//  Created by Nahid Islam on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct pull_me_urbanDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
